1bf3abb65bbe5d74c676eed72ac4bad0e106e5e2
ThePina <43052176+ThePina@users.noreply.github.com>
2021-05-09 17:24:13 -0400

    created admin function

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,5 @@
-const functions = require("firebase-functions");
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -7,3 +8,10 @@ const functions = require("firebase-functions");
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+exports.makeAdmin = functions.auth.user().onCreate((user) => {
+  if (user.email && user.email.endsWith('@admin.reservasalas.cl')) {
+    const customClaims = { admin: true };
+    return admin.auth().setCustomUserClaims(user.uid, customClaims);
+  }
+});
